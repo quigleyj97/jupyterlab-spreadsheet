8b3344b3695ddba5ca725fcba7c59e94dc386dd8
Joe Quigley <quigley.joseph@outlook.com>
2019-09-08 14:18:38 -0400

    WIP: Initial work on a Data Registry integration

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import { JupyterFrontEndPlugin, JupyterFrontEnd, ILayoutRestorer } from "@jupyterlab/application";
 import { IWidgetTracker, WidgetTracker } from "@jupyterlab/apputils";
+import { IRegistry, widgetDataType } from "@jupyterlab/dataregistry-extension";
 import { IDocumentWidget } from "@jupyterlab/docregistry";
 import { Token } from "@phosphor/coreutils";
 import { SpreadsheetModel } from "./model";
 import { SpreadsheetModelFactory } from "./modelfactory";
 import { SpreadsheetWidget } from "./widget";
 import { SpreadsheetWidgetFactory } from "./widgetfactory";
+import { createConverter, DataTypeNoArgs, resolveExtensionConverter } from "@jupyterlab/dataregistry";
+import { BoxLayout, Widget } from "@phosphor/widgets";
 
 export const ISpreadsheetTracker = new Token("jupyterlab-spreadsheet:tracker");
 export type ISpreadsheetTracker = IWidgetTracker<IDocumentWidget<SpreadsheetWidget, SpreadsheetModel>>;
 
-function activateSpreadsheet(app: JupyterFrontEnd,
-                             restorer: ILayoutRestorer): ISpreadsheetTracker {
+function activateSpreadsheet(
+    app: JupyterFrontEnd,
+    restorer: ILayoutRestorer,
+    registry?: IRegistry,
+): ISpreadsheetTracker {
     const tracker = new WidgetTracker<IDocumentWidget<SpreadsheetWidget, SpreadsheetModel>>({
         namespace: "jupyterlab-spreadsheet"
     });
@@ -39,10 +45,44 @@ function activateSpreadsheet(app: JupyterFrontEnd,
             ".xlsx"
         ],
         mimeTypes: [
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "application/vnd.ms-excel",
             "application/octet-stream",
             "text/plain"
         ]
     });
+    
+    if (registry) {
+        const XLSDatatype = new DataTypeNoArgs("application/vnd.ms-excel");
+        const XLSXDataType = new DataTypeNoArgs("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        
+        registry.addConverter(
+            createConverter({
+                from: XLSDatatype,
+                to: widgetDataType
+            }, () => {
+                return {
+                    type: "Spreadsheet",
+                    data: () => (new Widget())
+                }
+            }),
+            createConverter({
+                    from: XLSXDataType,
+                    to: widgetDataType
+            }, () => {
+                return {
+                    type: "Spreadsheet",
+                    data: () => (new Widget())
+                }
+            })
+        );
+
+        registry.addConverter(
+            resolveExtensionConverter(".xls", "application/vnd.ms-excel"),
+            resolveExtensionConverter(".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        )
+    }
+
     factory.widgetCreated.connect((sender, widget) => {
         tracker.add(widget);
         widget.context.pathChanged.connect(() => {
@@ -61,6 +101,7 @@ const plugin: JupyterFrontEndPlugin<ISpreadsheetTracker> = {
     id: "jupyter-spreadsheet",
     autoStart: true,
     requires: [ILayoutRestorer],
+    optional: [IRegistry],
     provides: ISpreadsheetTracker,
     activate: activateSpreadsheet
 };
